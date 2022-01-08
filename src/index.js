e37dff78e10f773a0ffb03a14db8f9fd7a69e4f9
Yaroslav Stetsiun <yaroslav.stetsuin@gmail.com>
2022-01-08 12:01:39 +0200

    feat: add function that convert number to human readable string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
+
+  if (number < 0 || typeof(number) !== 'number') return;
+  if (number === 0) return 'zero';
+
+  const str = number.toString();
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   
+  if (number < 20) {
+      return ones[number];
+  }
+
+  if (str.length === 2) {
+      return `${tens[str[0]]} ${ones[str[1]]}`.trim();
+  }
+
+  if (str.length === 3) {
+
+      if (str[1] === '0' && str[2] === '0') {
+          return `${ones[str[0]]} hundred`;
+      }
+
+      if (str[1] === '0') {
+          return `${ones[str[0]]} hundred ${ones[str[2]]}`
+      }
+
+      if (number % 100 < 20) {
+          return `${ones[str[0]]} hundred ${ones[number % 100]}`
+      }
+      
+      return `${ones[str[0]]} hundred ${tens[str[1]]} ${ones[str[2]]}`.trim();
+  }
+
 }
